91a65461238ae7c9b69c6a7e38d5b7cd85356156
danah olivetree <danah.olivetree@gmail.com>
2018-01-08 12:14:50 -0700

    trying bootstrap layout

--- src/components/nav/BuyOrSell.js
+++ src/components/nav/BuyOrSell.js
@@ -2,9 +2,21 @@ import React from 'react'
 import {NavLink} from 'react-router-dom'
 
 const BuyOrSell = () => (
-  <div>
-    <NavLink exact activeClassName='is-active' to='/shoppingFeed'><button type='button' className='btn btn-primary'>BUY</button></NavLink>
-    <NavLink exact activeClassName='is-active' to='/sell'><button type='button' className='btn btn-primary'>SELL</button></NavLink>
+  <div className='container'>
+    <div className='row justify-content-center'>
+      <h1 className="col-md-auto"> Welcome to Shit My Friends Make! Do you want to: </h1>
+    </div>
+    <div className='row justify-content-center'>
+      <div className="col-md-auto">
+        <NavLink exact activeClassName='is-active' to='/shoppingFeed'><button type='button' className='btn btn-primary'>BUY</button></NavLink>
+      </div>
+    </div>
+    <div className='row justify-content-center'>
+      <h3>or</h3>
+    </div>
+    <div className='row justify-content-center'>
+      <NavLink exact activeClassName='is-active' to='/sell'><button type='button' className='btn btn-primary'>SELL</button></NavLink>
+    </div>
   </div>
 )
 
