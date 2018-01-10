adb0bb8d0afde7c1938f63eccc138f9239c4937f
Danah Olivetree <danah.olivetree@gmail.com>
2018-01-10 12:48:07 -0700

    only shows relevant buttons

--- src/components/FeedItem.js
+++ src/components/FeedItem.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
-const FeedItem = ({item, onAddToCart, onAddBookmark}) => {
+const FeedItem = ({item, onAddToCart, onAddBookmark, path}) => {
+
 
 
   return (
 
     <div className="card">
-      <img className="card-img-top" src={item.image} alt='' />
+      <img className="card-img-top" src={item.image} width='200px' alt='' />
       <div className="card-block">
         <h4 className="card-title">{item.itemName}</h4>
         <h6 className="card-subtitle mb-2 text-muted">Seller: {item.sellerName}</h6>
@@ -14,8 +15,8 @@ const FeedItem = ({item, onAddToCart, onAddBookmark}) => {
           <h6 className="card-subtitle mb-2 text-muted">Quantity Available: {item.quantity} </h6>
         <p className="card-text">{item.description}</p>
         {/* <a href="#" className="btn btn-primary">link</a> */}
-        <input className='btn btn-primary' type='button' value='add to cart' onClick={onAddToCart} />
-        <input className='btn btn-primary' type='button' value='bookmark' onClick={onAddBookmark} />
+        { path !== 'shoppingcart' ? <input className='btn btn-primary' type='button' value='add to cart' onClick={onAddToCart} /> : '' }
+        { path !== 'bookmarks' ? <input className='btn btn-primary' type='button' value='bookmark' onClick={onAddBookmark} /> : '' }
 
       </div>
     </div>
